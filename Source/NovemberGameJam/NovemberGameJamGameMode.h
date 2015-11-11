27c2de5a1c4f66b575507ff0bd1f5dcb5108a6a8
Velhelm <voodoosoft2012@gmail.com>
2015-11-11 12:40:09 -0600

    Initial commit

---
+++ Source/NovemberGameJam/NovemberGameJamGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "NovemberGameJamGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOVEMBERGAMEJAM_API ANovemberGameJamGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
